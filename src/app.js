4598c9c6bbe77093e6e4eae664416e9c5142c4a6
jacobsamson <72816510+jacobsamson@users.noreply.github.com>
2022-08-16 17:04:19 -0700

    Styles Text

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ fetch('http://localhost:8000/results')
     .then(data => {
         var sumOfPrices = 0
         data.forEach(clothingDescription => {
+            //Gets each clothing description line and separates name and price
             clothingAttributes = clothingDescription.title.split('$',2);
             const name = clothingAttributes[0]
             const price = parseInt(clothingAttributes[1])
@@ -15,8 +16,9 @@ fetch('http://localhost:8000/results')
             feedDisplay.insertAdjacentHTML("beforeend", clothingInformation)
             console.log(sumOfPrices)    
         });
+        //Calculates average price of all clothing
         const averagePrice = sumOfPrices / data.length
         console.log(averagePrice)
-        const averagePriceText = `<div><h1>` + `The average price of clothing from this store is $` + averagePrice.toFixed(2) + `</h1></div>`
+        const averagePriceText = `<div><h2>` + `The average price of clothing from this store is $` + averagePrice.toFixed(2) + `</h2></div>`
         feedDisplay.insertAdjacentHTML("afterend",averagePriceText )
     })
